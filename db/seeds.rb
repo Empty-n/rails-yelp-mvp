8287f09d3d1952e297332b8bf3d79765a1bfe971
nb <nb.bischoff@gmail.com>
2021-11-10 15:15:48 +0100

    exo 2 done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+dishoom = Restaurant.create(name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: '0660779525', category: "chinese")
+pizza_east =  Restaurant.create(name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: '0388450660', category: "italian")
+epicure =  Restaurant.create(name: "Epicure", address: "112 Rue du Faubourg Saint-Honoré, 75008 Paris", phone_number: '0153434340', category: "french")
+le_bouillon_belge =  Restaurant.create(name: "Le bouillon belge", address: " 6 Rue Planchat, 75020 Paris", phone_number: '0143704103', category: "belgian")
+mizushi =  Restaurant.create(name: "Mizushi", address: " 46 Rue Saint-Georges, 75009 Paris", phone_number: '0182099744', category: "japanese")
